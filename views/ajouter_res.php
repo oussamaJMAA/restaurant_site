85b4b920df9d4536ec94bae0fe2f97fb5e95c391
Roukaia KHELIFI <69686634+RoukaiaKHELIFI@users.noreply.github.com>
2021-05-09 05:38:20 +0100

    email

--- views/ajouter_res.php
+++ views/ajouter_res.php
@@ -72,7 +72,7 @@ $mail->isSMTP();                            // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';             // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                     // Enable SMTP authentication
 $mail->Username = 'roukaia70@gmail.com';          // SMTP username
-$mail->Password = 'sousourourou9899@'; // SMTP password
+$mail->Password = 'test'; // SMTP password
 $mail->SMTPSecure = 'tls';                  // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                          // TCP port to connect to
 $mail->setFrom('roukaia70@gmail.com', 'roukaia khelifi');
